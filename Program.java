6951ad674084b263bedad335b7a5adb8e2bda42a
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2023-03-31 00:41:03 +0900

    Update Program.java

--- Program.java
+++ Program.java
@@ -6,7 +6,7 @@ public class Program {
 			System.out.println(1);
 			System.out.println(2);
 			System.out.println(3);
-// 프로그래밍을 통해 얻을 수 있는 것을 컴퓨터 프로그래밍 언어를 통해 순차적으로 해결할 수 있다.
+// 컴퓨터 프로그래밍 언어를 통해 순차적으로 해결할 수 있다.
 	}
 
 }
