bce1825a0219c55fcb7e6d23b4c4a7410886d589
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2023-11-23 02:11:28 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 124 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.js
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const [n, m] = fs.readFileSync('/dev/stdin').toString().trim().split(' ')
 
 const solution = (n, m) => {
-  return n === m ? n**2 - 1 : n * m - 1
+  return n * m - 1
 }
 console.log(solution(n, m))
