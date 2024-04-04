0d7f9c2baba1deeeb956b6967dc314bfdfc594e3
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-04-04 14:28:02 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.swift
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.swift
@@ -1,3 +1,6 @@
-func solution(_ num:Int) -> String {
-    return num % 2 == 0 ? "Even" : "Odd"
+func solution(_ number: Int) -> String {
+    if number % 2 == 0 {
+        return "Even"
+    }
+    return "Odd"
 }
