50fcb35490962edd8326cf70a7c9f019e5f013c7
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-04-06 21:58:35 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var answer:Int = 0
+    let arr = String(n).map { Int(String($0))! }
+    for i in 0...arr.count-1 {
+        answer += arr[i]
+    }
+    return answer
+}
