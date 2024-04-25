a7bd225ee512421dd18630346da91ff75f32b095
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-04-25 14:33:04 +0900

    [level 1] Title: 내적, Time: 0.42 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -1,9 +1,3 @@
 func solution(_ a: [Int], _ b: [Int]) -> Int {
-    var result = 0
-    
-    for i in 0..<a.count {
-        result += a[i] * b[i]
-    }
-    
-    return result
+    return zip(a, b).map { $0 * $1 }.reduce(0, +)
 }
