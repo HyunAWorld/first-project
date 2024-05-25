ed1e8086bb1477f4e7c91c6f38f1c46c7e74697c
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-05-25 22:39:42 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
@@ -1,10 +1,12 @@
-function solution(num1, num2) {
+// function solution(num1, num2) {
 
-    var answer = 0;
+//     var answer = 0;
 
-    answer=num1-num2
-        return answer;
-}
+//     answer=num1-num2
+//         return answer;
+// }
 
+
+let solution = (num1, num2) => num1-num2
 
 
