6964d901950f1f73f4e69b87324b819bf2924162
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2024-08-06 21:50:54 +0900

    [level 2] Title: 타겟 넘버, Time: 9.88 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,21 @@
 class Solution {
-    	public int solution(int[] numbers, int target) {
-       return dfs(numbers,target,0,0);
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
     }
-	// dfs 방식 ( 깊이 탐색 )
-	public int dfs (int numbers[], int target, int index, int sum) {
-		// 노드(인덱스)가 number의 길이와 같은 경우
-		if(index == numbers.length) {
-			if(sum == target) {
-				return 1;
-			}else {
-				return 0;
-			}
-		}
-		
-		int add = dfs(numbers,target,index+1,sum+numbers[index]);
-		int odd = dfs(numbers,target,index+1,sum-numbers[index]);
-		
-		return add + odd;
-	}// dfs fin
+    
+    // dfs 방식 (깊이 탐색)
+    public int dfs (int numbers[], int target, int index, int sum) {
+        // 노드(인덱스)가 number의 길이와 같은 경우
+        if(index == numbers.length) {
+            if(sum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        int add = dfs(numbers, target, index+1, sum+numbers[index]);
+        int odd = dfs(numbers, target, index+1, sum-numbers[index]);
+        
+        return add + odd;
+    } //dfs fin
 }
